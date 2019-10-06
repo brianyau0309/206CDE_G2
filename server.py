3e0625a8a73952af99a7f763ef4a3b107d20f68c
BriYaNa <brianyauu@gmail.com>
2019-10-06 18:28:39 +0800

    Update server.py, download and test socket.io
    Flask-Socket, eventlet

--- server.py
+++ server.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+from pathlib import Path
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'SecretKeyHERE!'
+socketio = SocketIO(app)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return render_template("index.html")
+
+@socketio.on('message')
+def handleMessage(msg):
+    print('Message: ', msg)
+    send(msg, broadcast=True)
+
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app, debug = True)
